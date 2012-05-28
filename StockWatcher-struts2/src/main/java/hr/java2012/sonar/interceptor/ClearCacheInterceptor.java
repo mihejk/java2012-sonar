a5aa322f02fec3194e9edab276d8b7a53d945c36
mihej <mihejk@gmail.com>
2012-05-28 19:35:06 +0200

    Isključeno cachiranje u browseru.

---
+++ StockWatcher-struts2/src/main/java/hr/java2012/sonar/interceptor/ClearCacheInterceptor.java
@@ -0,0 +1,26 @@
+package hr.java2012.sonar.interceptor;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts2.StrutsStatics;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+
+public class ClearCacheInterceptor extends AbstractInterceptor {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String intercept(final ActionInvocation invocation) throws Exception {
+		final ActionContext context = (ActionContext) invocation.getInvocationContext();
+		final HttpServletResponse response = (HttpServletResponse) context.get(StrutsStatics.HTTP_RESPONSE);
+		response.setHeader("Cache-Control", "no-cache, no-store");
+		response.setHeader("Pragma", "no-cache");
+		response.setDateHeader("Expires", 0);
+		final String result = invocation.invoke();
+		return result;
+	}
+
+}
